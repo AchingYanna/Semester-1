184bea778cb95bdf92000dbf5f97bef7d7051fa6
unused-desktop <chingang.elite@gmail.com>
2024-11-06 19:38:52 +0530

    Add files via upload

---
+++ Python/Practical Codes/Practica_8.py
@@ -0,0 +1,13 @@
+# Input strings and the number of characters to swap
+string1 = input("Enter the first string: ")
+string2 = input("Enter the second string: ")
+n = int(input("Enter the number of characters to swap: "))
+
+# Swap the first n characters if n is valid
+if n <= min(len(string1), len(string2)):
+    string1, string2 = string2[:n] + string1[n:], string1[:n] + string2[n:]
+    print("After swapping:")
+    print("First string:", string1)
+    print("Second string:", string2)
+else:
+    print("The value of n is too large for one of the strings.")
